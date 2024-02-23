b2d6121f620671f1ea8de0fa667953de0d06652a
kennedyifeadi <kennedyifeadi@gmail.com>
2024-02-23 22:16:07 +0100

    initial file commit

---
+++ mine.js
@@ -0,0 +1,32 @@
+const display = document.querySelector("#display");
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((item) => {
+  item.onclick = () => {
+    if (item.id == "clear") {
+      display.innerText = "";
+    } else if (item.id == "backspace") {
+      let string = display.innerText.toString();
+      display.innerText = string.substr(0, string.length - 1);
+    } else if (display.innerText != "" && item.id == "equal") {
+      display.innerText = eval(display.innerText);
+    } else if (display.innerText == "" && item.id == "equal") {
+      display.innerText = "Empty!";
+      setTimeout(() => (display.innerText = ""), 2000);
+    } else {
+      display.innerText += item.id;
+    }
+  };
+});
+
+
+
+const switchBtn = document.querySelector(".switch");
+const calculator = document.querySelector(".calculator");
+ const toggleIcon = document.querySelector(".icon");
+let isDark = true;
+switchBtn.onclick = () => {
+  calculator.classList.toggle("mode");
+  themeToggleBtn.classList.toggle("next");
+  isDark = !isDark;
+};
